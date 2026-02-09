c3b1683e7e7455b45e44bf65474f343e577bdc81
Naaji <aliamjadian.3330@gmail.com>
2026-02-09 21:27:49 +0330

    Update js.js

--- js.js
+++ js.js
@@ -248,11 +248,9 @@ function makeCall() {
 
 
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js")
-      .then(reg => console.log("Service Worker Registered"))
-      .catch(err => console.log("SW error:", err));
-  });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/rest/service-worker.js')
+    .then(() => console.log('SW registered'))
+    .catch(err => console.log('SW error', err));
 }
 
